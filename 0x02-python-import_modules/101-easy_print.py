91670714675bd702e841117dc3fa08d2e2a0b7b2
medelmir <medelmir001@gmail.com>
2023-06-09 07:31:15 -0700

    python

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    """Prints #pythoniscool followed by a new line"""
-    import os
-    os.write(1, b'#pythoniscool\n')
+import os
+os.write(1, b'#pythoniscool\n')
