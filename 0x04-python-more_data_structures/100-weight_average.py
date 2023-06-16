418de21f0cace3dcdd1e50004188e5430d24fcb3
medelmir <medelmir001@gmail.com>
2023-06-16 10:39:55 -0700

    python

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,6 +2,8 @@
 def weight_average(my_list=[]):
     x = 0
     y = 0
+    if my_list == []:
+        return 0
     for tuple in my_list:
         x = (tuple[0] * tuple[1]) + x
         y = tuple[1] + y
