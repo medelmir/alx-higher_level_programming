e582dd74089766e4134d0533e7a5b0c88188508f
medelmir <medelmir001@gmail.com>
2023-06-16 10:22:17 -0700

    python

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    x = 0
+    y = 0
+    for tuple in my_list:
+        x = (tuple[0] * tuple[1]) + x
+        y = tuple[1] + y
+    average = x / y
+    return average
