70cfc885e7401bf5a4f5dc95ed215e64a0e75517
medelmir <medelmir001@gmail.com>
2023-07-03 12:45:35 -0700

    python

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """__init__ method.
+        Args:
+            width (int): integer width
+            height (int): integer height
+        """
+
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Get/set the width of the rectangle."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get/set the height of the rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("heigh must be an integer")
+        if value < 0:
+            raise ValueError("heigh must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Return the perimeter of the rectangle."""
+        if self.__width == 0 or self.__height == 0:
+            return (0)
+        return ((2 * self.__width) + (2 * self.__height))
+
+    def __str__(self):
+        """Print the rectangle with the character #."""
+        if self.__width == 0 or self.__height == 0:
+            return ("")
+        else:
+            string = ""
+            for i in range(self.__height):
+                string += "#" * self.__width
+                if i < self.__height - 1:
+                    string += "\n"
+            return (string)
+
+    def __print__(self):
+        """Print the rectangle with the character #."""
+        if self.__width == 0 or self.__height == 0:
+            print("")
+        else:
+            for i in range(self.__height):
+                print("#" * self.__width) 
