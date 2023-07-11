bc38a28c88950e88b5f23aa395d7a63fc8790c78
medelmir <medelmir001@gmail.com>
2023-07-11 13:58:41 -0700

    python

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""function that reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """reads a text file"""
+    with open(filename, "r") as file:
+        print(file.read(), end="\n")
