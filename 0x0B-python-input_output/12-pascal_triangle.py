ac9cfb96d45359a77752b7f4985f2459293d0143
medelmir <medelmir001@gmail.com>
2023-07-11 16:08:27 -0700

    python

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -5,9 +5,7 @@ triangle of n"""
 
 
 def pascal_triangle(n):
-     """function def pascal_triangle(n): that returns a list
-    of lists of integers representing the Pascal’s
-    triangle of n"""
+     """function def pascal_triangle(n)"""
     if n <= 0:
         return []
     if n == 1:
