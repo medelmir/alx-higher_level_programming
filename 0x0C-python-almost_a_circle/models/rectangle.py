aba7e4ad367ef22332475b10d7e45bb011083c32
medelmir <medelmir001@gmail.com>
2023-07-16 10:30:31 -0700

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -72,3 +72,9 @@ class Rectangle(Base):
         """prints in stdout the Rectangle instance with#"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """ returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
+        x = f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+        y = f"{self.__width}/{self.__height}"
+        return x + y
