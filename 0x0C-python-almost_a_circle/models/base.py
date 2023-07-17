99fdb0462aa4b0ca5541dbe12f4a4a6d8fe9755f
medelmir <medelmir001@gmail.com>
2023-07-17 12:00:18 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Defines a base model class."""
+import json
 
 
 class Base:
@@ -12,4 +13,10 @@ class Base:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = Base.__nb_object
+
+    def to_json_string(list_dictionaries):
+        """Return the JSON serialization of a list of dicts."""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)s
