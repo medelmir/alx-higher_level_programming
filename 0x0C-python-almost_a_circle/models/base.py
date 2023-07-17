ddfdf066c34b6d27f36c8b5b3b53807ac7397cfc
medelmir <medelmir001@gmail.com>
2023-07-17 12:02:54 -0700

    python

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,14 +9,15 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initialize a new Base."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_object
+            self.id = Base.__nb_objects
 
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of a list of dicts."""
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)s
+        return json.dumps(list_dictionaries)
