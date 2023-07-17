e6ecaf0fe4ed93de7d7c2c972e2d677209e04870
medelmir <medelmir001@gmail.com>
2023-07-17 11:13:59 -0700

    python

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a square model class."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initialize a new square."""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """Return a string representation of this square."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.width)
