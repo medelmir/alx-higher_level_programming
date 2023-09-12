cae6a14b99291e9d19bcb329ff008ab4f6385440
medelmir <medelmir001@gmail.com>
2023-09-12 09:27:06 -0700

    javascript

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -9,15 +9,4 @@ function factorial (n) {
   return (n * factorial(n - 1));
 }
 
-console.log(factorial(Number(process.argv[2])));#!/usr/bin/node
-function factorial (n) {
-  if (n < 0) {
-    return (-1);
-  }
-  if (n === 0 || isNaN(n)) {
-    return (1);
-  }
-  return (n * factorial(n - 1));
-}
-
 console.log(factorial(Number(process.argv[2])));
